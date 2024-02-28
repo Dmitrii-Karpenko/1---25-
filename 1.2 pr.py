7acb0bebc7b6be0736943e4738bc9dec40c6ed5e
rEmber1-md-20 <ilya.rember@gmail.com>
2024-02-28 13:00:18 +0300

    test

--- 1.2 pr.py
+++ 1.2 pr.py
@@ -3,5 +3,7 @@ if int(nomer) <= 36 and int(nomer) % 2 == 0:
     print('Ваше место верхнее, купе')
 elif int(nomer) <= 36 and int(nomer) % 2 != 0:
     print('Ваше место нижнее, купе')
+elif int(nomer) > 36 and int(nomer) % 2 != 0:
+    print('Ваше место боковое, верхнее')
 else:
-    print('Ваше место боковое')
+    print('Ваше место боковое, нижнее')
