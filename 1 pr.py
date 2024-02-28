978cc1c8251fcb8b864d4ad1b2382216a8d2a25d
rEmber1-md-20 <ilya.rember@gmail.com>
2024-02-28 12:25:19 +0300

    test

--- 1 pr.py
+++ 1 pr.py
@@ -1,1 +1,7 @@
-print('sgdfgdfg')
+password = input('Введите пароль')
+Repassword = input('Введите пароль повторно')
+
+if password == Repassword:
+    print('Пароль принят')
+else:
+    print('Пароль не принят')
